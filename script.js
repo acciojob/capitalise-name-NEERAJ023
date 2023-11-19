562f06f567d6c78c02c232f1d0304b9c1267ad58
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-19 14:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let input=document.querySelector("#fname");
+
+input.addEventListener("blur",()=>{
+	blr();
+})
+function blr(){
+	let input=document.querySelector("#fname");
+let value=input.value;
+	input.value=value.toUpperCase();
+}
